73228950558de9e4d5055959f0d43dbb92713426
Andrea Turrini <andrea.turrini@outlook.com>
2024-06-03 10:35:53 +0800

    Run smc script

---
+++ PacPMA/runsmc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+file=$1
+property=$2
+parameters=$3
+constants=$4
+call=(java -jar pacpma.jar -f $file -p "`echo $property`" --model-checker stormsmcc --parameters $parameters --lpsolver matlab --seed 12345 --format matlab --log-level 4 --logfile pacpma.log --degree 2 --showrange)
+if [ $constants ]; then
+	call+=(--constants $constants)
+fi
+"${call[@]}"
